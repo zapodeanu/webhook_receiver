6f7eac6585d638235f53880bf427f9af4b259dc7
gzapodea <gzapodea@cisco.com>
2019-11-20 13:36:01 -0800

    initial commit

--- test_webhook_receiver.py
+++ test_webhook_receiver.py
@@ -29,20 +29,68 @@ __license__ = "Cisco Sample Code License, Version 1.1"
 
 import requests
 import json
-import datetime
 import urllib3
-import base64
-import time
 from requests.auth import HTTPBasicAuth  # for Basic Auth
 from config import WEBHOOK_URL, WEBHOOK_USERNAME, WEBHOOK_PASSWORD
 
 from urllib3.exceptions import InsecureRequestWarning  # for insecure https warnings
+
 urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
 
+basic_auth = HTTPBasicAuth(WEBHOOK_USERNAME, WEBHOOK_PASSWORD)
 
-dnac_param = {"version": "", "instanceId": "84bc5a0d-b211-4c50-81e0-a142da540d45", "eventId": "NETWORK-NON-FABRIC_WIRED-1-200", "namespace": "ASSURANCE", "name": "", "description": "", "type": "NETWORK", "category": "ALERT", "domain": "Connectivity", "subDomain": "Non-Fabric Wired", "severity": 1, "source": "ndp", "timestamp": 1569449708000, "tags": "", "details": {"Type": "Network Device", "Assurance Issue Details": "This network device PDX-3850-CAMPUS is unreachable from controller. The device role is ACCESS", "Assurance Issue Priority": "P1", "Device": "10.93.130.47", "Assurance Issue Name": "Network Device 10.93.130.47 Is Unreachable From Controller", "Assurance Issue Category": "Availability", "Assurance Issue Status": "active"}, "ciscoDnaEventLink": "dna/assurance/issueDetails?issueId=84bc5a0d-b211-4c50-81e0-a142da540d45", "note": "To programmatically get more info see here - https://<ip-address>/dna/platform/app/consumer-portal/developer-toolkit/apis?apiId=8684-39bb-4e89-a6e4", "tntId": "", "context": "", "tenantId": ""}
+dnac_param = {"version": "",
+              "instanceId": "84bc5a0d-b211-4c50-81e0-a142da540d45",
+              "eventId": "NETWORK-NON-FABRIC_WIRED-1-200",
+              "namespace": "ASSURANCE",
+              "name": "", "description": "",
+              "type": "NETWORK",
+              "category": "ALERT",
+              "domain": "Connectivity", "subDomain": "Non-Fabric Wired",
+              "severity": 1,
+              "source": "ndp",
+              "timestamp": 1574274686000,
+              "tags": "",
+              "details": {"Type": "Network Device",
+                          "Assurance Issue Details": "This network device PDX-3850-CAMPUS is unreachable from controller. The device role is ACCESS",
+                          "Assurance Issue Priority": "P1",
+                          "Device": "10.93.130.47",
+                          "Assurance Issue Name": "Network Device 10.93.130.47 Is Unreachable From Controller",
+                          "Assurance Issue Category": "Availability",
+                          "Assurance Issue Status": "active"},
+              "ciscoDnaEventLink": "dna/assurance/issueDetails?issueId=84bc5a0d-b211-4c50-81e0-a142da540d45",
+              "note": "To programmatically get more info see here - https://<ip-address>/dna/platform/app/consumer-portal/developer-toolkit/apis?apiId=8684-39bb-4e89-a6e4",
+              "tntId": "",
+              "context": "",
+              "tenantId": ""}
 
-sdwan_param = {"devices": [{"system-ip": "10.1.0.1"}], "eventname": "system-reboot-issued", "type": "system-reboot-issued", "rulename": "system-reboot-issued", "component": "System", "entry_time": 1569479153138, "statcycletime": 1569479153138, "message": "The device rebooted", "severity": "Medium", "severity_number": 3, "uuid": "31832379-ee8d-4b8f-a1d8-1aaf745649c2", "values": [{"host-name": "DC1-VEDGE1", "site-id": 200, "system-ip": "10.1.0.1", "reboot-reason": "Initiated by user"}], "rule_name_display": "System_Reboot_Issued", "receive_time": 1569479153138, "values_short_display": [{"host-name": "DC1-VEDGE1", "system-ip": "10.1.0.1"}], "acknowledged": False, "cleared_events": ["0519b356-7def-4b4d-babf-9171380e0e82"], "active": False}
+sdwan_param = {"devices": [{"system-ip": "21.21.21.21"}],
+               "eventname": "interface-admin-state-change",
+               "type": "interface-admin-state-change",
+               "rulename": "interface-admin-state-change",
+               "component": "VPN",
+               "entry_time": 1574274686000,
+               "statcycletime": 1574274686000,
+               "message": "The interface admin-state changed to down",
+               "severity": "Critical",
+               "severity_number": 1,
+               "uuid": "303af097-12ca-4aa2-b1e8-544094a7c96d",
+               "values":
+                   [{"host-name": "vBond-Pod5",
+                     "system-ip": "21.21.21.21",
+                     "if-name": "eth0",
+                     "new-admin-state": "down",
+                     "vpn-id": "512"}],
+               "rule_name_display": "Interface_Admin_State_Change",
+               "receive_time": 1574274686635,
+               "values_short_display":
+                   [{"host-name": "vBond-Pod5",
+                     "system-ip": "21.21.21.21",
+                     "if-name": "eth0",
+                     "new-admin-state": "down"}],
+               "acknowledged": False,
+               "active": True}
+
 
 def pprint(json_data):
     """
@@ -62,8 +110,6 @@ print(response_json)
 
 """
 
-basic_auth = HTTPBasicAuth(WEBHOOK_USERNAME, WEBHOOK_PASSWORD)
-
 # test the Webhook with a Cisco DNA Center notification
 
 url = WEBHOOK_URL
@@ -72,17 +118,17 @@ response = requests.post(url, auth=basic_auth, data=json.dumps(dnac_param), head
 response_json = response.json()
 print('\n', response_json)
 
-# print the HTTP BasicAuth encoding - needed for Cisco DNA Center webhook configuration
-print('\nThe HTTP Basic Auth you will need for the Webhooks Configuration is:\n' + response.request.headers['Authorization'], '\n')
 
-"""
-time.sleep(5)
 # test the Webhook with a Cisco SD-WAN notification
 
 url = WEBHOOK_URL
 header = {'content-type': 'application/json'}
 response = requests.post(url, auth=basic_auth, data=json.dumps(sdwan_param), headers=header, verify=False)
+print(response.status_code)
 response_json = response.json()
-print(response_json)
+print('\n', response_json)
 
-"""
+
+# print the HTTP BasicAuth encoding - needed for Cisco DNA Center webhook configuration
+print('\nThe HTTP Basic Auth you will need for the Webhooks Configuration is:\n' + response.request.headers[
+    'Authorization'])
